db9c08dfd9ba33f2ef169a19aa7f8f5c0d301c3c
R189 <31823582+rox97@users.noreply.github.com>
2022-01-17 10:28:06 +0100

    Implementazione iniziale corso e studente

--- src/main/java/it/univr/elearning/Course.java
+++ src/main/java/it/univr/elearning/Course.java
@@ -1,8 +1,37 @@
 package it.univr.elearning;
 
+import javax.persistence.*;
+
+@Entity
 public class Course {
 
-    private StudentRepository studentRepository;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String courseName;
+    private String coordinatorName;
+    //TODO: come usare persistence e le repository
+    //Servono per il json e le tabelle del database: non può essere una repository perché non può essere una colonna
+    //private StudentRepository studentRepository;
+
+    protected Course(){}
+
+    public Course(String courseName, String coordinatorName){
+        this.courseName = courseName;
+        this.coordinatorName = coordinatorName;
+    }
+
+
+    public Long  getId() {return id;}
+
+    public String getCourseName(){return courseName;}
+
+    public String getCoordinatorName(){return coordinatorName;}
+
+
+
 
 
 }
--- src/main/java/it/univr/elearning/Student.java
+++ src/main/java/it/univr/elearning/Student.java
@@ -1,4 +1,19 @@
 package it.univr.elearning;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+
+
 }
