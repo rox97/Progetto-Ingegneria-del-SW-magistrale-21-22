e0c6ddc61cd95927754baac1dd40fd8dda02da1e
Andrea Caliari <andreacaliari87@gmail.com>
2022-02-15 14:08:51 +0100

    Aggiunti test selenium su addPoll e addFile lato studente

--- src/test/java/it/univr/elearning/SystemTest.java
+++ src/test/java/it/univr/elearning/SystemTest.java
@@ -128,26 +128,66 @@ public class SystemTest extends BaseTest {
 
     @Test
     public void testAddFile(){
-        driver.get("http://localhost:8080/login");
-        String message1 = driver.findElement(By.tagName("h1")).getText();
-        assertEquals("Login message expected", "Login", message1);
-        driver.findElement(By.name("userName")).sendKeys("qwerty");
-        driver.findElement(By.name("password")).sendKeys("alessandro");
-        driver.findElement(By.name("password")).submit();
-        String message2 = driver.findElement(By.tagName("h1")).getText();
-        assertEquals("First row should be 'Courses'", "Courses", message2);
+        //init();
+        testLoginStudent();
+
+        driver.findElement(By.xpath("/html/body/form/input")).click(); // Entro nella pagina di archiviazione personale lato studente
+        String title1 = driver.findElement(By.tagName("h2")).getText();
+        assertEquals("First row should be 'Cloud Personale'", "Cloud Personale", title1); //Controllo di essere entrato nel corso giusto
+
+        WebElement uploadElement = driver.findElement(By.xpath("/html/body/section/div/div/div/form[1]/div/input[2]"));
+
+        uploadElement.sendKeys(System.getProperty("user.dir")+ "\\src\\test\\resources\\fileTest\\Course-project.pdf"); //Seleziono il file da caricare
+        driver.findElement(By.xpath("/html/body/section/div/div/div/form[1]/button")).click(); // Faccio upload del file
+        title1 = driver.findElement(By.xpath("/html/body/section/div/div/div/p")).getText();
+
+        String[] trimmedText = title1.split("==>");
+        assertEquals("First row should be 'File caricato con successo '", "File caricato con successo ", trimmedText[0]); //Controllo che il file sia stato caricato
+        //driver.findElement(By.xpath("/html/body/section/div/div/div/table/tbody/tr/td[2]/form/input[3]")).click(); // Elimino il file
+        driver.findElement(By.xpath("/html/body/section/div/div/div/form[2]/input")).click(); // Torno alla pagina dei corsi
+
+        title1 = driver.findElement(By.tagName("h1")).getText();
+        assertEquals("First row should be 'Courses'", "Courses", title1); //Controllo di essere tornato alla pagina dei corsi
+
+
     }
+
 
     @Test
     public void testPoll(){
-        driver.get("http://localhost:8080/login");
-        String message1 = driver.findElement(By.tagName("h1")).getText();
-        assertEquals("Login message expected", "Login", message1);
-        driver.findElement(By.name("userName")).sendKeys("qwerty");
-        driver.findElement(By.name("password")).sendKeys("alessandro");
-        driver.findElement(By.name("password")).submit();
-        String message2 = driver.findElement(By.tagName("h1")).getText();
-        assertEquals("First row should be 'Courses'", "Courses", message2);
+        //init();
+        testLoginProfessor();//Login con credenziali professore
+
+        driver.findElement(By.xpath("/html/body/table/tbody/tr/td[4]/a")).click(); // Entro nella pagina del corso
+        String title1 = driver.findElement(By.tagName("h1")).getText();
+        assertEquals("First row should be 'Fondamenti di Ingegneria del SW'", "Fondamenti di Ingegneria del SW", title1); //Controllo di essere entrato nel corso giusto
+        driver.findElement(By.xpath("/html/body/a[4]/button")).click(); //Entro nella pagina del sondaggio
+
+        title1 = driver.findElement(By.tagName("h1")).getText();
+        assertEquals("First row should be 'Create a new Survey'", "Create a new Survey", title1); //Controllo di essere entrato nella pagina sondaggio
+        driver.findElement(By.name("title")).sendKeys("Sondaggio qualità del corso"); // Inserisco titolo sondaggio
+        driver.findElement(By.xpath("/html/body/form/input[2]")).click(); //Clicco sulla checkbox isMandatory
+
+        driver.findElement(By.xpath("/html/body/form/input[4]")).click(); //Clicco sul salva
+        title1 = driver.findElement(By.xpath("/html/body/div/label")).getText();
+        assertEquals("First row should be 'Answer Survey:'", "Answer Survey:", title1); //Controllo di essere nella pagina di creazione delle domande
+
+        driver.findElement(By.xpath("/html/body/div/input")).sendKeys("Ritiene che il materiale fornito sia sufficente per seguire le lezioni?"); //Inserimento dati sondaggio
+        driver.findElement(By.xpath("/html/body/form[1]/div/input[2]")).sendKeys("No");
+        driver.findElement(By.xpath("/html/body/button[1]")).click();
+        driver.findElement(By.xpath("/html/body/form[1]/div[2]/input[2]")).sendKeys("Abbastanza");
+        driver.findElement(By.xpath("/html/body/button[1]")).click();
+        driver.findElement(By.xpath("/html/body/form[1]/div[3]/input[2]")).sendKeys("Si");
+        driver.findElement(By.xpath("/html/body/form[2]/input[2]")).click(); //Clicco fine per salvare il sondaggio
+
+        title1 = driver.findElement(By.tagName("h1")).getText();
+        assertEquals("First row should be 'Fondamenti di Ingegneria del SW'", "Fondamenti di Ingegneria del SW", title1); //Controllo di essere tornato alla pagina del corso
+
+        driver.findElement(By.xpath("/html/body/form/input")).click(); //Clicco fine per tornare alla pagina dei corsi
+        title1 = driver.findElement(By.tagName("h1")).getText();
+        assertEquals("First row should be 'Courses'", "Courses", title1);
+
+
     }
 
     @Test
